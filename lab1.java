a327895c5492531c71b33c51d434dd81e7c61cf8
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-10-04 06:15:27 -0700

    Add files via upload

---
+++ lab1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Arrays;
+
+
+public class lab1 {
+	
+	public static void main() {
+		int key;
+		int[] a = new int[100];
+		
+		Random rand = new Random():
+		
+		//part A
+		
+		for(int i =1; i < 100; i++) {
+			key = rand.nextInt(100);
+			binarySearch();
+			
+		}
+	}
+
+}
