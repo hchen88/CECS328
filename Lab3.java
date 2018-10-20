a76256def5ceaa8401f91e256aec860cd0c6673a
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-10-20 10:33:04 -0700

    Add files via upload

--- Lab3.java
+++ Lab3.java
@@ -3,26 +3,71 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Lab3 {
-	
-	public static int[] quick_sort(int[] array, int start, int end) {
-		if(start < end) {
-			partitioned = partition(array, start, end);
-			
-			quickSort(array, low, partitioned -1);
-			quickSort(array, partitioned + 1, high);
+
+	//needs work
+	public static int partition (int[] array, int start, int end) {
+		//figure out pivot
+		int midpoint = (int) Math.ceil(array.length/2);
+		int pivotValue = 0, temp, pivotIndex = 0;
+		int k = start -1;
+		//median of the first, middle, and last 
+		if(array[end] >= array[midpoint] && array[start] <= array[midpoint]
+			|| array[end] <= array[midpoint] && array[start] >= array[midpoint]	) {
+			pivotValue = array[midpoint];
+			pivotIndex = midpoint;
 		}
-			
+		if(array[midpoint] >= array[end] && array[end] >= array[start] ||
+				array[midpoint] <= array[end] && array[end] <= array[start]) {
+			pivotValue = array[end];
+			pivotIndex = end;
 		}
-		return n; 
+		
+		if(array[midpoint] <= array[start] && array[end] >= array[start] ||
+				array[midpoint] >= array[start] && array[end] <= array[start]) {
+			pivotValue = array[start];
+			pivotIndex = start;
+		}
+		
+		System.out.println("Pivot: " + pivotValue + "Pivout Index: " + pivotIndex);
+		
+		
+		//swap pivot with last index.	
+		temp = array[array.length-1];
+		array[array.length-1] = array[pivotIndex];
+		array[pivotIndex] = array[end];
+
+		
+		for( int i = start; i < end; i++) {
+			if( array[i] >= pivotValue) {
+				k++;
+				
+			}
+		}
+//		
+//		temp = array[k+1];
+//		array[k+1] = array[end];
+//		array[end] = temp;
+		
+		return k+1; //returns partition index.
 	}
 	
-	public int partition (int[] array, int low, int high) {
-		//figure out pivot
-		int midpoint = array.length/2;
-		//median of the first, middle, and last 
-		if(array[0] > array[midpoint]) {
-			
+	
+	public static void sort(int[] array, int start, int end)  {
+		if (start < end)
+		{
+			int partitionIndex = partition(array, start, end);
+			//recursive call dividing array
+			sort(array, start, partitionIndex -1);
+			sort(array, partitionIndex+1, end);
 		}
+		
+	}
+
+	public static int[] quick_sort(int[] array) {
+		sort(array, 0, array.length-1); 
+		return array;
+	}
+	
 		
 /**
  * this method takes an unsorted integer array of any size and sorts the array from least to greatest.
@@ -58,24 +103,43 @@ public class Lab3 {
 		double start, stop, insertionTime = 0.00, quickSortTime = 0.00;
 		int n, totalTimes = 100;
 		
-		Random r = new Random(); 
-		System.out.println("Please enter in a positive integer :");
-		nString = in.nextLine().trim();
-		n = Integer.parseInt(nString);
-		int[] array = new int[n];
-		start = System.nanoTime();
-		int[] sortedArray = insertion_sort(array);
-		stop = System.nanoTime();
-		insertionTime = stop - start;
-		System.out.println("Average Time to Sort Random array with Insertion Sort: " + insertionTime/totalTimes + " nanoseconds" );
-		System.out.println("Average Time to Sort Random array with Insertion Sort: " + insertionTime/totalTimes + " nanoseconds" );
 		
-
-
+		//tests arrays
+		int[] array = { -1, -2, 0 , 2, 3, 4, -2, 100};
+		System.out.print("Unsorted: [");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + ", ");
+		}
+		System.out.print("]\n");
 		
-//		for( int i = 0; i < n -1; i++) {
-//			array[i] = r.nextInt(5000 + 1 + 5000) - 5000;
-//		}
+		int[] sortedArray = quick_sort(array);
+		System.out.print("Sorted: [");
+		for (int i = 0; i < sortedArray.length; i++) {
+			System.out.print(sortedArray[i] + ", ");
+		}
+		System.out.print("]");
+		
+		
+//		Random r = new Random(); 
+//		System.out.println("Please enter in a positive integer :");
+//		nString = in.nextLine().trim();
+//		n = Integer.parseInt(nString);
+//		int[] array = new int[n];
+//		start = System.nanoTime();
+//		int[] sortedArray = insertion_sort(array);
+//		stop = System.nanoTime();
+//		insertionTime = stop - start;
+//		System.out.println("Average Time to Sort Random array with Insertion Sort: " + insertionTime/totalTimes + " nanoseconds" );
+//		
+//		start = System.nanoTime();
+//		sortedArray = quick_sort(array);
+//		stop = System.nanoTime();
+//		quickSortTime = stop - start;
+//		System.out.println("Average Time to Sort Random array with Quick Sort Time: " + quickSortTime/totalTimes + " nanoseconds" );
+//		
+		
+		
+		
 		
 
 	}
